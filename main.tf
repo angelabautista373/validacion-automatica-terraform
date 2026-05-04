17d4fb23e55186f15a9a9a08f6f8e727384052ba
Angela Bautistaa <antovaleria1@gmail.com>
2026-05-04 15:41:00 +0200

    Deploy final con LocalStack funcionando

--- main.tf
+++ main.tf
@@ -15,13 +15,12 @@ provider "aws" {
   region                      = "us-east-1"
 
   s3_use_path_style           = true
-
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://127.0.0.1:4566"
+    s3 = "http://localhost:4566"
   }
 }
 
